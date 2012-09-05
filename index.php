857839ddfc0e77c514c8614bc48794d582f81d95
Paul Nurkkala <pnurkkal@winbuild1.cse.taylor.edu>
2012-09-04 20:04:25 -0400

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>TU Capstone Study 2012</title>
+	</head>
+	<body>
+		<p>Welcome to the TU computer science capstone studying website.</p>
+		<p><b>Who are you?</b> (Whatever you say here
+		will be chached and reported to everyone else)</p>
+		<select value="">
+		  <option value="">Person 1</option>
+		  <option value="">Person 2</option>
+		</select>
+		<br/>
+		<a href="#">Modify Questions</a>
+	</body>
+</html>
